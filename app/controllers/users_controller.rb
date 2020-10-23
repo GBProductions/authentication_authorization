9877e83542905919a96e9079ff41d0809aeebb80
Garrett Brown <garrettpaulbrown@gmail.com>
2020-10-23 15:41:37 -0700

    Add text forgotten, fix typos in users_controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,13 @@ class UsersController < ApplicationController
     end
 
     def create
-      @user = User.new
+      @user = User.new(user_params)
       if @user.save
-        flash[:notice] = "you've successfully signed up!"
-        session[:user_id] = @user.user_id
+        flash[:notice] = "You've successfully signed up!"
+        session[:user_id] = @user.id
+        redirect_to "/"
+      else
+        flash[:alert] = "There was a problem signing up."
         redirect_to '/signup'
       end
     end
