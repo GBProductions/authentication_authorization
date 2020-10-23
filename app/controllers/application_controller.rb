df64d2268d7d58def9f18782453b0ae165b09159
Garrett Brown <garrettpaulbrown@gmail.com>
2020-10-23 12:44:34 -0700

    Add more controllers.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,11 @@ class ApplicationController < ActionController::Base
   def current_user
     if session[:user_id]
       @current_user ||= User.find(session[:user_id])
+
+  def authorize
+    if !current_user
+      flash[:alert] = "You aren't authorized to visit that page."
+      redirect_to '/'
+    end
+  end
 end
